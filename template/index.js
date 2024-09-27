1c76759a91b6b6d4c5646b01af03d5d6c340e44f
ryudedev <2210441@ecc.ac.jp>
2024-09-28 01:28:27 +0900

    :bug: fix: Bug fixes in conditional expressions

--- template/index.js
+++ template/index.js
@@ -7,13 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, world!' });
 });
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-if (process.env.NODE_ENV !== 'test') {
-  const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-
-  module.exports = { app, server }; // サーバーをエクスポート
-} else {
-  module.exports = { app };
-}
+module.exports = { app, server }; // サーバーをエクスポートß
